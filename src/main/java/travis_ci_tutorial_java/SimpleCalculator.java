d4153277cfba46583aa01e7e53dab161257640e5
BNay248 <bradleynaylon@yahoo.com>
2025-03-05 17:30:25 -0500

    add mult and div methods

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -7,4 +7,10 @@ public class SimpleCalculator {
 	public int sub(int a, int b) {
 		return a - b;
 	}
+	public int mult(int a, int b) {
+		return a * b;
+	}
+	public int div(int a, int b) {
+		return a / b;
+	}
 }
