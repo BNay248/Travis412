dd2cfbd65d0531489735e6818faa2a75de262742
BNay248 <bradleynaylon@yahoo.com>
2025-03-05 17:34:15 -0500

    test div by zero

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -26,4 +26,9 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.div(8, 4), 2);
 	}
+	@Test (expected = java.lang.ArithmeticException.class)
+	public void testDivByZero() {
+		SimpleCalculator calc = new SimpleCalculator();
+		calc.div(8, 0);
+	}
 }
